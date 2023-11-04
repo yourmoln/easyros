ffcfde4ce3a0595e767b7261763d487c1694480a
yourmoln <yourmoln@outlook.com>
2023-11-04 23:04:58 +0800

    0.0.2 fix:no choose

--- easyros/__init__.py
+++ easyros/__init__.py
@@ -3,7 +3,8 @@ script_path = os.path.split(os.path.realpath(__file__))[0]
 sys.path.append(script_path)
 from install import rosInstall
 def run():
-    rosInstall()
-    print("自动安装已结束")
+    if input('Do you want to auto install ros?  (y/n)') == 'y':
+        rosInstall()
+        print("自动安装已结束")
 if __name__ == "__main__":
     run()
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from easyros import run
 
 if __name__ =='__main__':
-    if input('Do you want to auto install ros?  (y/n)') == 'y':
-        run()
+    run()
