265e58eb18bbcea8beb3e7684aabc0623d0116c9
yourmoln <yourmoln@outlook.com>
2023-11-05 20:08:05 +0800

    0.0.9 fix again

--- easyros/rostool/__init__.py
+++ easyros/rostool/__init__.py
@@ -1,6 +1,6 @@
 from .evar import *
 import os
 script_path = os.path.split(os.path.realpath(__file__))[0]
-def creat(name):
+def creat(name,path):
     os.system(f"chmod 777 {script_path}/creat.sh")
-    os.system(f"{script_path}/creat.sh {name}")
+    os.system(f"{script_path}/creat.sh {name} {path}")
